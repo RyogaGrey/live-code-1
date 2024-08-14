320eb3eb944d384ff1d574465bea2b5c93d82dcc
RyogaGrey <sergryvolk86@gmail.com>
2024-08-14 16:32:53 +0500

    ...

--- src/Components/FormContext.tsx
+++ src/Components/FormContext.tsx
@@ -1,19 +1,4 @@
 import React, { createContext, useState, ReactNode } from 'react';
-import * as crypto from "crypto";
-
-function generate(length: number = 16) : string {
-    const uppercase: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase: string = "abcdefghijklmnopqrstuvwxyz";
-    const numbers: string = "0123456789";
-    const symbols: string = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-    const all: string = uppercase + lowercase + numbers + symbols;
-    let password: string = "";
-    for (let index: number = 0; index < length; index++) {
-      const randomNumber: number = crypto.randomInt(all.length);
-      password += all.charAt(randomNumber);
-    }
-    return password;
-}
 
 interface FormData {
     name: string;
